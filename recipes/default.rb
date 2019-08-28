3adbbd7df802d94002ba8eb60991f5f3d03357d2
Matthew Mackreth <MMackreth@spartaglobal.com>
2019-08-28 15:29:41 +0100

    Added installation of filebeats

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,26 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+
+# COPY SSL CERTIFICATE
+
+directory '/etc/pki/tls/certs' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+apt_repository 'filebeat' do
+  uri 'https://packages.elastic.co/beats/apt stable main'
+  key 'https://packages.elastic.co/GPG-KEY-elasticsearch'
+  trusted false
+  components ['filebeat']
+end
+
+package 'filebeat'
+
+template '/etc/filebeat/filebeat.yml' do
+  source 'filebeat.yml'
+end
