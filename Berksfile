4dc548f37685f92fe9e6a6316ca952a6d1f3ad12
Matthew Mackreth <MMackreth@spartaglobal.com>
2019-09-02 16:38:49 +0100

    Added filebeats cookbook

---
+++ Berksfile
@@ -0,0 +1,3 @@
+source 'https://supermarket.chef.io'
+
+cookbook 'filebeat', '~> 2.2.0'
--- recipes/default.rb
+++ recipes/default.rb
@@ -10,13 +10,15 @@ end
 execute 'Move certificate and install filebeats' do
   command 'mkdir -p /etc/pki/tls/certs'
   command 'sudo cp /tmp/logstash-forwarder.crt /etc/pki/tls/certs/'
-  command 'wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -'
-  command 'sudo apt-get install apt-transport-https'
-  command 'echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list'
-  command 'sudo apt-get update && sudo apt-get install filebeat'
-  command 'sudo apt-get update'
-  command 'sudo apt-get install filebeat'
+  # command 'wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -'
+  # command 'sudo apt-get install apt-transport-https'
+  # command 'echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list'
+  # command 'sudo apt-get update && sudo apt-get install filebeat'
+  # command 'sudo apt-get update'
+  # command 'sudo apt-get install filebeat'
 end
+
+filebeat_install 'default'
 
 template '/etc/filebeat/filebeat.yml' do
   source 'filebeat.yml.erb'
